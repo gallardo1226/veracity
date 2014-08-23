0e068185134d161836aeb148187f3b85fe387992
Noah Conley <gallardo1226@gmail.com>
2014-08-23 10:45:13 -0400

    working framework, ready for code/styles to be added

--- veracity/routes/index.js
+++ veracity/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res) {
+	res.render('about', { title: 'About Us' })
+});
+
+router.get('/contact', function(req, res) {
+	res.render('contact', { title: 'Contact Us' })
+});
+
+router.get('/campuslife', function(req, res) {
+	res.render('campus_life', { title: 'Campus Life' })
+});
+
+router.get('/journey', function(req, res) {
+	res.render('journey', { title: 'Journey' })
+});
+
+router.get('/lifeandculture', function(req, res) {
+	res.render('life_and_culture', { title: 'Life and Culture' })
 });
 
 module.exports = router;
