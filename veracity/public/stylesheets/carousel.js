ea6e5e1b52b2096b3c92a542428069bef06ec336
Phoebe Kim <kim_hj14@yahoo.com>
2014-10-01 17:41:30 -0500

    added carousel template

---
+++ veracity/public/stylesheets/carousel.js
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+    jQuery(function ($) {
+        $('#carousel').carousel({
+            interval: 2000
+        });
+
+        var clickEvent = false;
+
+        $('#carousel').on('click', '.nav a', function () {
+            clickEvent = true;
+            $('.nav li').removeClass('active');
+            $(this).parent().addClass('active');
+        }).on('slid.bs.carousel', function (e) {
+            if (!clickEvent) {
+                var count = $('.nav').children().length - 1;
+                var current = $('.nav li.active');
+                current.removeClass('active').next().addClass('active');
+                var id = parseInt(current.data('slide-to'));
+                if (count == id) {
+                    $('.nav li').first().addClass('active');
+                }
+            }
+            clickEvent = false;
+        });
+    });
+</script>
