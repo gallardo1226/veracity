d32857fd1013e2c41e4a196fe929937d562cbff1
Noah Conley <gallardo1226@gmail.com>
2014-11-25 19:55:45 -0600

    stuff

--- veracity/app.js
+++ veracity/app.js
@@ -1,26 +1,22 @@
 var express = require('express');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
+// var session = require('express-session');
+// var MongoStore = require('connect-mongo')(session);
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
-var secret = require('password-generator');
+// var secret = require('password-generator');
 
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     relationship = require('mongoose-relationship');
 mongoose.connect('mongodb://localhost:27017/veracity');
 var db = mongoose.connection;
-db.on('open', function () {
-    app.listen(3002);
-    console.log("connection open");
-});
-
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
+    console.log("connection open");
 
     var userSchema = mongoose.Schema({
         name: {
@@ -92,10 +88,11 @@ var server = app.listen(3000, function() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req,res,next){
--- veracity/routes/index.js
+++ veracity/routes/index.js
@@ -3,27 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    // console.log("global load");
     res.render('index', { title: 'About Us' });
-    // var previous      = req.session.value || 0;
-    // req.session.value = previous + 1;
-    // res.end('<h1>Previous value: ' + previous + '</h1>');
-    // res.send(req.session);
 });
-
-router.get("/request",function(req,res){
-    console.log("request received");
-    console.log(req.session);
-    var m=req.session.isLogged || 0;
-    req.session.isLogged = m+1;
-    console.log(req.session.isLogged);
-});
-
-router.get("/getsession",function(req,res){
-    console.log("getsession received");
-    console.log(req.session);
-    console.log(req.session.isLogged);
-})
 
 router.get('/about', function(req, res) {
 	res.render('public/about', { title: 'About Us' });
