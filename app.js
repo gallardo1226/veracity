91df8375bb00418f986b5c13b0af352ee06dc994
Noah Conley <gallardo1226@gmail.com>
2014-12-05 00:31:39 -0600

    port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var mongoose = require('mongoose'),
 var app = express();
 
 console.log(process.env);
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
